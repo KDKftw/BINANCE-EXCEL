c23377016c74b569a722bce85c8ec8e00f7c7239
KDKftw <ooo.kadoun@gmail.com>
2021-08-22 00:36:55 +0200

    no message

--- the big guy.py
+++
@@ -1,7 +0,0 @@
-from FUNCTIONS import *
-
-pathTradeExport = r"C:\Users\KDK\Desktop\FOR testing binc.xlsx"
-wb = load_workbook(pathTradeExport)
-ws = wb.worksheets[1]
-ws.cell(row=x, column=6).value= getTotalpaid(x)
-wb.save(pathTradeExport)
